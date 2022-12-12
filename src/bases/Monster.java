1b0d26fd65551c9c7c1ce99cc4318d9245df679c
Akira_Matsushima <119549568+Akira-Matsushima-Yell@users.noreply.github.com>
2022-12-12 09:18:10 +0900

    一旦push

---
+++ src/bases/Monster.java
@@ -0,0 +1,26 @@
+package bases;
+
+import utils.Dice;
+
+// モンスターを表現する抽象クラス
+public abstract class Monster extends Living {
+
+	// コンストラクタ
+	public Monster(String name, String weapon) {
+		// Livingクラスで定義したコンストラクタを利用する
+		super(name, weapon );
+	}
+
+	// attackメソッドのオーバーライド
+	@Override
+	public void attack(Living target) {
+		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
+		int num = Dice.get(1, 11) * getOffensive();
+		// 相手のHPをダメージ値だけ減らす
+		int da = target.getHp()-num;
+		//
+		System.out.println(this.name +"が　" + this.weapon +"で攻撃!" +target +"に" + num + "のダメージを与えた。" );
+	
+		
+	}
+}
